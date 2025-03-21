c5e3907c9695657de56c4679ca272ac677a9cfe8
Iryna Kosogor <iryna.kosogor@dataart.com>
2025-03-21 18:17:24 +0200

    methods to convert objects

---
+++ 04 - Object Methods/methods_to_convert_objects.rb
@@ -0,0 +1,16 @@
+text = "5"
+puts text.to_i + 12
+puts text.class
+puts text.to_f + 12
+puts text.class
+
+puts 12.to_s
+
+puts "15 apples".to_i
+puts "apples 15".to_i
+
+puts text.to_f
+
+number = 5
+puts number.class
+puts number.to_s.class
