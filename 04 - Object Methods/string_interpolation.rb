c39f61ded9a04a7a944cb8804a00884be01e2086
Iryna Kosogor <iryna.kosogor@dataart.com>
2025-03-21 17:29:02 +0200

    string interpolation

---
+++ 04 - Object Methods/string_interpolation.rb
@@ -0,0 +1,12 @@
+name = "irina"
+puts "Hello #{name.capitalize}, how are you?"
+
+age = 37
+years_to_add = 15
+puts "I am #{age} years old"
+
+puts "In #{years_to_add } years, I will be #{age + years_to_add} years old!"
+
+x = 5 
+y = 8
+puts "The sum of #{x} and #{y} is equal to #{x + y}"
