41750f8c5dfc65c2413304eb74159e690de99895
Iryna Kosogor <iryna.kosogor@dataart.com>
2025-03-22 11:46:21 +0200

    Return values

---
+++ 06 - Methods/return_values_i.rb
@@ -0,0 +1,8 @@
+def add_two_numbers(num1, num2)
+  puts "OK, I'm solving your math problem"
+  # return "Just kidding, I'm a troll!"
+  return num1 + num2
+end
+
+result = add_two_numbers(1, 5)
+p result
