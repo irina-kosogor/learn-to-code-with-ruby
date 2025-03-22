2288b3364b467794797c7c247ef40a40fbf39145
Iryna Kosogor <iryna.kosogor@dataart.com>
2025-03-22 11:21:02 +0200

    Multiple parameters

---
+++ 06 - Methods/multiple_parameters.rb
@@ -0,0 +1,8 @@
+def praise_person(name, trait)
+ puts "#{name} is #{trait}!"
+end
+
+praise_person("Iryna", "amazing")
+praise_person("Iryna", "charming")
+praise_person("Iryna", "talented")
+# praise_person("Iryna") ArgumentError
